f643b13bd56311c427dfd19917c04ba206e3d2a4
Dale Hewgill <dhewgill@gmail.com>
2015-12-23 14:29:07 -0800

    ADC10 setup and enable.  Will allow for 'random' values to be generated for the PWM values.

--- main.c
+++ main.c
@@ -16,13 +16,17 @@
 #define F_MCLK		16000000ul
 #define F_SMCLK		16000000ul
 
-#define SYSFLG_UPDATE_PWM	0x80
+#define SYSFLG_UPDATE_PWM		0x80
+#define SYSFLG_ADC_DONE			0x40
+
 
 /* Function Prototypes */
 static inline void configPort1(void);
 static inline void configTimerA(void);
 static inline void configWDT(void);
+static inline void configAdc10(void);
 static inline void updatePwm(volatile uint16_t * ta0_ccr_reg, int8_t * incr);
+static inline void updateTargetVals(void);
 
 
 /* Type Definitions */
@@ -32,10 +36,10 @@ static inline void updatePwm(volatile uint16_t * ta0_ccr_reg, int8_t * incr);
 const uint16_t G_MIN_TIMER_VAL = 128;
 const uint16_t G_MAX_TIMER_VAL = 65408;
 
-
 volatile uint8_t g_sys_flags;
 int8_t g_channel_incr[NUM_CHANNELS] = {8, 8, 8};
-
+uint16_t g_channel_target_vals[NUM_CHANNELS] = {G_MIN_TIMER_VAL, G_MIN_TIMER_VAL, G_MIN_TIMER_VAL};
+uint16_t g_raw_adc10_vals[NUM_CHANNELS] = {0, 0, 0};
 
 int main(void)
 {
@@ -60,7 +64,14 @@ int main(void)
     		g_sys_flags &= ~SYSFLG_UPDATE_PWM;
     	}
 
-    	__bis_SR_register(LPM0_bits | GIE);
+    	if (g_sys_flags & SYSFLG_ADC_DONE)
+    	{
+    		updateTargetVals();
+    		g_sys_flags &= ~SYSFLG_ADC_DONE;
+    	}
+
+    	if (g_sys_flags == 0)
+    		__bis_SR_register(LPM0_bits | GIE);
     }
 
     return 0;
@@ -114,6 +125,27 @@ static inline void configTimerA(void)
 	TA0CTL = TASSEL_2 | MC_2 | TACLR | TAIE;
 }
 
+/*
+ * Configure the ADC10:
+ * - SMCLK source, divider /8 [~2MHz]
+ * - Channel = Temperature sensor
+ * - SW start of conversion
+ *
+ * - Internal Reference
+ * - 64 clocks SHT [~32us/sample]
+ * - REF = 1.5V
+ * - ADC10 on
+ * - ADC10 interrupt enable
+ */
+static inline void configAdc10(void)
+{
+	ADC10DTC0 = 0;
+	ADC10DTC1 = NUM_CHANNELS;
+	ADC10CTL1 = INCH_10 | ADC10DIV_7 | ADC10SSEL_3 | CONSEQ_2;
+	ADC10CTL0 = SREF_1 | ADC10SHT_3 | MSC | REFON | ADC10ON | ADC10IE | ENC | ADC10SC;
+	ADC10SA = &g_raw_adc10_vals[0];
+}
+
 
 /*
  * Update the Timer0 CCRs to shift the PWM point.
@@ -139,7 +171,29 @@ static inline void updatePwm(volatile uint16_t * ta0_ccr_reg, int8_t * incr)
 }
 
 
+/*
+ * Function used to update the PWM target values.
+ * It is run when the ADC10 has finished a block
+ * of conversions.
+ * Once the next targets are ready, then shut off
+ * the ADC10.
+ */
+static inline void updateTargetVals(void)
+{
+	register uint8_t i;
+
+	for (i=0; i<NUM_CHANNELS; i++)
+	{
+		g_channel_target_vals[i] <<= 1;
+		g_channel_target_vals[i] |= (g_raw_adc10_vals[i] & 0x0001);
+	}
+
+	ADC10SA = &g_raw_adc10_vals[0];			// Start the next conversions.
+}
+
+
 /* Interrupt Routines */
+// Watchdog interrupt service routine - handles WDT overflow [interval mode].
 #pragma vector=WDT_VECTOR
 __interrupt void WDT_ISR(void)
 {
@@ -152,6 +206,7 @@ __interrupt void WDT_ISR(void)
 	}
 }
 
+// Timer0_A1 interrupt service routine - handles TA0 overflow.
 #pragma vector=TIMER0_A1_VECTOR
 __interrupt void TIMER0_A1_ISR(void)
 {
@@ -167,4 +222,13 @@ __interrupt void TIMER0_A1_ISR(void)
 
 	// Now read TA0IV to get rid of the interrupt.
 	TA0IV;
+}
+
+// ADC10 interrupt service routine
+// Will fire once all three <g_raw_adc_vals> are filled.
+#pragma vector=ADC10_VECTOR
+__interrupt void ADC10_ISR (void)
+{
+	g_sys_flags |= SYSFLG_ADC_DONE;
+	__bic_SR_register_on_exit(LPM0_bits);
 }
